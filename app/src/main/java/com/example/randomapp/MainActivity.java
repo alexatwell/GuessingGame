61573788c9084d38de790e39213ffbf61657533d
alexatwell <alexatwell246@gmail.com>
2021-12-15 23:06:39 -0400

    Commit #2
    Basic Functionality Complete
    
    Tasks
    - UI to be revised
    - Overlays to be completed
    - Instructions Dialog (settings button)
    - Difficulty / Level (settings button) - optional

--- app/src/main/java/com/example/randomapp/MainActivity.java
+++ app/src/main/java/com/example/randomapp/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -15,24 +17,47 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     public int randVal;
     public int guessVal;
+    public int tries = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText guess =  findViewById(R.id.guess);
         final AppCompatButton guessBtn = findViewById(R.id.guessBtn);
+        final Button randomBtn = (Button) findViewById(R.id.randomBtn);
+        final TextView guessView = findViewById(R.id.guessView);
+        final TextView welcomeView = findViewById(R.id.welcomeView);
+        final EditText editGuess =  findViewById(R.id.editGuess);
 
         ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
         progressDialog.setMessage("Loading...");
 
+        randomBtn.setOnClickListener(v -> {
+
+            //progressDialog.show();
+
+            Random rand = new Random(); //instance of random class
+            int upperbound = 30;
+            //generate random values from 1-30
+            int int_random = rand.nextInt(upperbound);
+            //double double_random=rand.nextDouble();
+            //float float_random=rand.nextFloat();
+            randVal = int_random + 1;
+
+            randomBtn.setVisibility(View.INVISIBLE);
+            welcomeView.setVisibility(View.INVISIBLE);
+            guessBtn.setVisibility(View.VISIBLE);
+            editGuess.setVisibility(View.VISIBLE);
+            guessView.setText(String.valueOf(tries));
+        });
+
         guessBtn.setOnClickListener(v -> {
 
             //progressDialog.show();
 
-            final String guessText = guess.getText().toString();
+            final String guessText = editGuess.getText().toString();
 
             if (guessText.isEmpty()){
                 Toast.makeText(MainActivity.this, "Entry Required!", Toast.LENGTH_SHORT).show();
@@ -41,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 try{
                     guessVal = Integer.parseInt(guessText);
                     guessValue();
+                    guessView.setText(String.valueOf(tries));
                 } catch (NumberFormatException e) {
                     Toast.makeText(MainActivity.this, "Integer Required!", Toast.LENGTH_SHORT).show();
                     progressDialog.dismiss();
@@ -51,17 +77,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void generateButton(View view) {
-
-        Random rand = new Random(); //instance of random class
-        int upperbound = 30;
-        //generate random values from 1-30
-        int int_random = rand.nextInt(upperbound);
-        //double double_random=rand.nextDouble();
-        //float float_random=rand.nextFloat();
-        randVal = int_random + 1;
-    }
-
 
     public void guessValue() {
         if(guessVal == randVal){
@@ -70,6 +85,10 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "Too Cold", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(MainActivity.this, "Too Hot", Toast.LENGTH_SHORT).show();
+        }
+        tries--;
+        if (tries <= 0){
+            System.exit(0);
         }
     }
 }
